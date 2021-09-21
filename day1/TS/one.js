9b43bea012193e72788a84231286fa47bbc1ea45
RejoiceGrass <sikaile888@163.com>
2021-09-21 15:14:40 +0800

    master

---
+++ day1/TS/one.js
@@ -0,0 +1,7 @@
+(function () {
+    function sayHi(str) {
+        return '你好啊' + str;
+    }
+    var text = '飘柔的小草';
+    console.log(sayHi(text));
+})();
---
+++ day1/TS/one.ts
@@ -0,0 +1,7 @@
+(()=>{
+    function sayHi(str:string){
+        return '你好啊' + str
+    }
+    let text = '飘柔的小草'
+    console.log(sayHi(text))
+})()
