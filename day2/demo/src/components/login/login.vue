784cc21bfb2755cecda3a1c12ab6d8f89cad1bd5
RejoiceGrass <sikaile888@163.com>
2021-09-22 01:07:37 +0800

    login

---
+++ day2/demo/src/components/login/login.vue
@@ -0,0 +1,84 @@
+<template>
+<div class="firstdemo">
+<el-form ref="form" :model="form" label-width="80px">
+<el-row type="flex" justify="center">
+
+<el-col :span="5">
+<el-form-item label="用户名">
+<el-input v-model="form.name"></el-input>
+</el-form-item>
+</el-col>
+</el-row>
+
+<el-row type="flex" justify="center">
+<el-col :span="5">
+<el-form-item label="密码">
+<el-input v-model="form.password"></el-input>
+</el-form-item>
+</el-col>
+</el-row>
+<el-row type="flex" justify="center">
+<el-col :span="5">
+<el-form-item>
+<el-button type="primary" @click="onSubmit">登陆</el-button>
+<el-button>注册</el-button>
+</el-form-item>
+</el-col>
+</el-row>
+</el-form>
+</div>
+</template><script>
+export default {
+  name: "fisrtdemo",
+  data() {
+    return {
+      form: {
+        name: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    onSubmit() {
+      if (this.form.name == "admin" && this.form.password == "123456") {
+        this.$message({
+          message: "登陆成功",
+          type: "success",
+        });
+        this.$router.push({ path: "/Home" });
+      } else {
+        this.$message.error("登陆失败");
+      }
+
+//       this.$refs.loginForm.validate(valid => {
+//  if (valid) {
+//   axios.post('/login', {
+//   user: this.loginForm.user,
+//   pass: this.loginForm.pass
+//   })
+//   .then((response) => {
+//    if (response.status === 200) {
+//    this.$store.commit('SET_TOKEN', response.data.token)
+//    this.$store.commit('GET_USER', response.data.user)
+//    this.$message({
+//     message: '登陆成功',
+//     type: 'success'
+//    })
+//    this.$router.push({name: 'activity'})
+//    }
+//   })
+//   .catch(function (error) {
+//    console.log(error)
+//   })
+//  } else {
+//   console.log('error submit!!')
+//   return false
+//  }
+//  })
+
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped></style>
--- day2/demo/src/router/index.ts
+++ day2/demo/src/router/index.ts
@@ -1,11 +1,22 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import login from "../components/login/login.vue";
 const routes: Array<RouteRecordRaw> = [
   {
+    //登录
+    path: "/",
+    name: "login",
+    component: login
+    },
+  {
+    //主页
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    //登录拦截
+    meta: {
+      requireAuth: true
+    },
   },
   {
     path: '/about',
@@ -16,6 +27,20 @@ const routes: Array<RouteRecordRaw> = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
+// routes.beforeEach((to, from, next) => {
+//   if (to.matched.some(res => res.meta.requireAuth)) {// 判断是否需要登录权限
+//     if (localStorage.getItem('token')) {// 判断是否登录
+//       next()
+//     } else {// 没登录则跳转到登录界面
+//       next({
+//         path: '/Login',
+//         query: {redirect: to.fullPath}
+//       })
+//     }
+//   } else {
+//     next()
+//   }
+// })
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
